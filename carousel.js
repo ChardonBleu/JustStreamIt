0658dd23f7bcc37ad58acc391ff3fef520b17f8b
Fabienne Rondi <f.rondi@orange.fr>
2021-05-26 16:56:43 +0200

    Ajouts commentaires dans carousels.js

--- carousel.js
+++ carousel.js
@@ -3,7 +3,10 @@
 
 "use strict";
 
-
+/**
+ * Active carousel navigation and resize
+ * @param  {object} carousel - carousel's DOM
+ */
 function activeSlide(carousel) {
 
     const slider = carousel.getElementsByClassName('carousel__slider')[0],
@@ -14,42 +17,64 @@ function activeSlide(carousel) {
     let width = 300,
         height = 300,
         totalWidth = 20,
-        margin = 20,
-        currIndex = 0;
+        margin = 30,
+        currentIndex = 0;
 
+    /**
+     * Adapt carousel boxes dimentions and adjust  apparent number of boxes
+     */
     function resize() {
-        height = Math.max(window.innerWidth * 0.18, 300);
-        width = height * 182 / 266 ;
-        totalWidth = width * items.length;
+        console.log(window.innerWidth);
+        width =  Math.max((window.innerWidth * 0.685 - 3 * margin) / 4, 182);
+        height = width / 0.8;
+
+        totalWidth = width * items.length + 7 * margin;
 
         slider.style.width = totalWidth + "px";
 
         for (const item of items) {
-            item.style.width = (width - (margin * 2)) + "px";
+            item.style.width = width + "px";
             item.style.height = height + "px";
         }
 
     }
 
+    /**
+     * calculation of the translation when clik on navigation arrows for
+     * each carousel
+     * @param  {number} index - box index
+     */
     function move(index) {
         if (index < 1) {
             index = (items.length - 1);
         } else if (index > (items.length - 2)) {
             index = 1;
         }
-        currIndex = index;
-        slider.style.transform = "translateX(" + ((index-1) * -width) + "px)";
+        currentIndex = index;
+        slider.style.transform = "translateX(" + ((index-1) * (-width - margin)) + "px)";
 
     }
 
+    /**
+     * decrease the index box and ask for a move with this new index
+     */
     function prev() {
-        move(--currIndex);
+        move(--currentIndex);
     }
 
+    /**
+     * increase the index box and ask for a move with this new index
+     */
     function next() {
-        move(++currIndex);
+        move(++currentIndex);
     }
 
+    /**
+     * Wait for events:
+     * window resize --> carousels boxes resize too
+     * clik on previous arrow to navigate in slider
+     * clik on next arrow to navigate in slider
+     */
     function bindEvents() {
         window.addEventListener("resize", resize);
         prevBtn.addEventListener('click', prev);
@@ -65,14 +90,12 @@ function activeSlide(carousel) {
     init();
 
 }
-var carousel1 = document.getElementById('carousel0');
-activeSlide(carousel0);
 
-var carousel1 = document.getElementById('carousel1');
-activeSlide(carousel1);
-
-var carousel1 = document.getElementById('carousel2');
-activeSlide(carousel2);
-
-var carousel1 = document.getElementById('carousel3');
-activeSlide(carousel3);
+/**
+ * Activation of the four carousels
+ * @arg {Object} - carousel's DOM
+ */
+activeSlide(document.getElementById('carousel0'));
+activeSlide(document.getElementById('carousel1'));
+activeSlide(document.getElementById('carousel2'));
+activeSlide(document.getElementById('carousel3'));
--- modal.js
+++ modal.js
@@ -6,11 +6,19 @@ var bestButton = document.getElementsByClassName("best__details__button")[0];
 var modal = document.getElementById("myModal");
 const imgs = document.getElementsByClassName("carousel__slider__item__img");
 
+
+/**
+ * Hide modal window
+ */
 function modalClose() {
     'use-strict';
     modal.style.display = 'none';
 }
 
+/**
+ * Show modal window for carousel's box
+ * @param  {Object} event
+ */
 function ModaleCarousel(event) {
     'use-strict';
     modal.style.display = "flex";
@@ -18,6 +26,9 @@ function ModaleCarousel(event) {
     modalImg.src = event.target.src;
 }
 
+/**
+ * show modal window for best film
+ */
 function ModaleBest() {
     'use-strict';
     modal.style.display = "flex";
@@ -26,10 +37,19 @@ function ModaleBest() {
     modalImg.src = bestImg.src;
 }
 
+/**
+* Waiting for modal window closure
+*/
 span.addEventListener('click', modalClose);
 
+/**
+* Waiting for modal window for the best film
+*/
+bestButton.addEventListener('click', ModaleBest);
+
+/**
+* Waiting for modal window for each carousel's films
+*/
 for (const img of imgs) {
     img.addEventListener('click', ModaleCarousel)
  }
-
-bestButton.addEventListener('click', ModaleBest);
