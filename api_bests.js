b7a85fad9213ab2f73890b6857bbf1b2484137eb
Fabienne Rondi <f.rondi@orange.fr>
2021-05-26 10:18:31 +0200

    Ajout des docstrings dans api_best.js

--- api_bests.js
+++ api_bests.js
@@ -1,12 +1,24 @@
 /*jslint es6*/
 /*global document window fetch console*/
 
+/*This file has in charge the requests for the best film in imdb score*/
+
+/**
+ * Add new paragraph inside parent document given in arguments.
+ * @param  {Object} parent_document - design tags in witch paragraph have to be inserted
+ * @param  {String} paragraph - contains integral string with <p> tags.
+ */
 function newParagraph(parent_document, paragraph) {
     'use-strict';
     const newElt = document.createElement("p");
     parent_document.appendChild(newElt).innerHTML = paragraph;
 }
 
+/**
+ * Search best film details using the id_film given in argument.
+ * Add film informations in html document.
+ * @param  {number} id_film - best film id in API datas
+ */
 function detailsBestFilm(id_film) {
     'use-strict';
     fetch("http://localhost:8000/api/v1/titles/" + id_film)
@@ -30,15 +42,26 @@ function detailsBestFilm(id_film) {
 
 }
 
-function bestFilm(values, index) {
+/**
+ * Use the first value of promise's data given in argument values.
+ * Put the image of this film in concerned html tags.
+ * send id_film to another function.
+ * @param  {Object} values - promise's object from principal request
+ */
+function bestFilm(values) {
     'use-strict';
-    const image_url = values.results[index].image_url;
-    const best_img = document.getElementsByClassName("best__img")[index];
+    const image_url = values.results[0].image_url;
+    const best_img = document.getElementsByClassName("best__img")[0];
     best_img.src = image_url;
-    const id_film = values.results[index].id;
+    const id_film = values.results[0].id;
     detailsBestFilm(id_film);
 }
 
+
+/**
+ * Principal requests searching for the 5 bests IMDb score films.
+ * Promise's object is send to the function bestFilm.
+ */
 fetch("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&page=1")
     .then(function (response) {
         'use-strict';
@@ -48,7 +71,7 @@ fetch("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&page=1")
     })
     .then(function (values) {
         'use-strict';
-        bestFilm(values, 0);
+        bestFilm(values);
     })
     .catch(function (error) {
         'use-strict';
