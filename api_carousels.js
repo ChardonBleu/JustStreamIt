e940567b0506a7d3944227ca44f2281e58eb01aa
Fabienne Rondi <f.rondi@orange.fr>
2021-05-26 09:59:04 +0200

    Ajout des docstings dans api_carousels.js

--- api_carousels.js
+++ api_carousels.js
@@ -1,10 +1,15 @@
 /*jslint es6*/
 /*global document window fetch console*/
 
-/**
- * @param  {} values
- * @param  {number} carouselIndex
+/*This file has in charge the API requests for carousels film images*/
 
+/**
+ * Select the carousel witch number is done in arguments. the carousel is
+ * selected by its class attribute: carousel + carouselIndex.
+ * Receive in arguments a request promise witch contains films images.
+ * Put this images in the carousel's five first boxes.
+ * @param  {Object} values - request's promise
+ * @param  {number} carouselIndex
  */
 function fiveFirstImagesCarousel(values, carouselIndex) {
     'use-strict';
@@ -19,6 +24,13 @@ function fiveFirstImagesCarousel(values, carouselIndex) {
     }
 }
 
+/**
+ * Select the carousel witch number is done in arguments.
+ * Receive in arguments a request promise witch contains films images.
+ * Put this images in the carousel's last two boxes.
+ * @param  {Object} values
+ * @param  {number} carouselIndex
+ */
 function twoLastImagesCarousel(values, carouselIndex) {
     'use-strict';
     const carouselItems = document
@@ -32,6 +44,14 @@ function twoLastImagesCarousel(values, carouselIndex) {
     }
 }
 
+/**
+ * Launch two requests on API with search option given in argument.
+ * the first request is on page one, and the second in on page two.
+ * Each request contains data of five films.
+ * Promise's object and carousel's index are send to functions.
+ * @param  {Number} carouselIndex - carousel's number concerned by the requests.
+ * @param  {String} request - option of search for the request
+ */
 function carouselRequest(carouselIndex, request) {
     'use-strict';
     fetch("http://127.0.0.1:8000/api/v1/titles/?" + request + "&page=1")
@@ -61,7 +81,34 @@ function carouselRequest(carouselIndex, request) {
         });
 }
 
+/**
+ * Concern request for the fisrt carousel whitch shown
+ * the 7 best films in imdb score.
+ * @arg  {number} - carousel's number concerned by the requests.
+ * @arg  {String} - search option of the request
+ */
 carouselRequest(0, "sort_by=-imdb_score");
+
+/**
+ * Concern request for the second carousel whitch shown
+ * the 7 Science fiction bests votes films of.
+ * @arg  {number} - carousel's number concerned by the requests.
+ * @arg  {String} - search option of the request
+ */
 carouselRequest(1, "genre=sci-Fi&sort_by=-votes");
+
+/**
+ * Concern request for the third carousel whitch shown
+ * the 7 Comedy bests votes films.
+ * @arg  {number} - carousel's number concerned by the requests.
+ * @arg  {String} - search option of the request
+ */
 carouselRequest(2, "genre=comedy&sort_by=-votes");
+
+/**
+ * Concern request for the fourth carousel whitch shown
+ * the 7 Thriller bests votes films.
+ * @arg  {number} - carousel's number concerned by the requests.
+ * @arg {String} - search option of the request
+ */
 carouselRequest(3, "genre=thriller&sort_by=-votes");
